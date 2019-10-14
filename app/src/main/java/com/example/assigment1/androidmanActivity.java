a0cc6d8ff3f316628220e92dedbd9b93fad1166a
ahmed islam <ahmedislam.23@gmail.com>
2019-10-14 21:57:54 +0200

    Initial commit

---
+++ app/src/main/java/com/example/assigment1/androidmanActivity.java
@@ -0,0 +1,14 @@
+package com.example.assigment1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class androidmanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_androidman);
+    }
+}
